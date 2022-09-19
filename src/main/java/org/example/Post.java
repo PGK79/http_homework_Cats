e0cbaf8233b674392db014016d6b1364378c82cd
PGK79 <pgk_nev@mail.ru>
2022-09-19 22:09:58 +0300

    Добавлены геттеры и сеттеры в Photo

--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -1,22 +1,58 @@
 package org.example;
 
 public class Post {
-    public String id;
-    public String text;
-    public String type;
-    public String user;
+    private String id;
+    private String text;
+    private String type;
+    private String user;
     private String upvotes;
 
     public Post() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
     }
 
     public String getUpvotes() {
         return upvotes;
     }
 
+    public void setUpvotes(String upvotes) {
+        this.upvotes = upvotes;
+    }
+
     @Override
     public String toString() {
-        return "Employee{" + "id='" + id + '\'' + ", text='" +
+        return "Post{" + "id='" + id + '\'' + ", text='" +
                 text + '\'' + ", type='" + type + '\'' +
                 ", user='" + user + '\'' + ", upvotes='" + upvotes + '\'' + '}';
     }
