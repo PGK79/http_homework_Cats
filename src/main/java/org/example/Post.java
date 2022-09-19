a1a4cad08b319cfafc63e1dbe3226aa192b9738e
PGK79 <pgk_nev@mail.ru>
2022-09-19 10:14:34 +0300

    format Post.java

--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -5,22 +5,15 @@ public class Post {
     public String text;
     public String type;
     public String user;
-    public String upvotes;
+    private String upvotes;
 
     public Post() {
     }
 
-//    public Post(String id, String text, String type, String user,int upvotes) {
-//        this.id = id;
-//        this.text = text;
-//        this.type = type;
-//        this.user = user;
-//        this.upvotes = upvotes;
-//    }
-
     public String getUpvotes() {
         return upvotes;
     }
+
     @Override
     public String toString() {
         return "Employee{" + "id='" + id + '\'' + ", text='" +
